2da3aafa2639cbdde25f245213f8f79cf12ca383
Faizan Patel <faizan2786@hotmail.com>
2024-05-16 12:57:30 +0100

    'conform.lua' config used for auto format on saving

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,7 +1,4 @@
-local null_ls = require('null-ls') -- import the null-ls plugin
-
--- defina a group for configuring auto-format on SAVE
-local auto_group = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require "null-ls" -- import the null-ls plugin
 
 -- configure the null_ls sources (i.e built-in tools)
 local opts = {
@@ -9,35 +6,10 @@ local opts = {
   -- list null-ls sources(i.e. language tools) to use
   sources = {
     null_ls.builtins.formatting.clang_format, -- c++ formatting tool
-    null_ls.builtins.formatting.black,    -- python formatting tool
-    null_ls.builtins.diagnostics.mypy,    -- load null-ls' diagnostics tools
-    null_ls.builtins.diagnostics.ruff,    -- 'mypy' and 'ruff' for static type
-                                          -- checking and linting respectively.
-    null_ls.builtins.formatting.prettier, -- formatting tool for html, css, js
+    null_ls.builtins.formatting.black, -- python formatting tool
+    null_ls.builtins.diagnostics.mypy, -- load null-ls' diagnostics tools
+    null_ls.builtins.diagnostics.ruff, -- 'mypy' and 'ruff' for static type
+    -- checking and linting respectively.
   },
-
-  -- configure auto-format on save
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      -- clear any exisiting auto commands
-      vim.api.nvim_clear_autocmds({
-        group = auto_group,
-        buffer = bufnr,
-      })
-
-      -- create our custom auto command for the event that triggers
-      -- just before writing buffers (i.e. before saving writes to files)
-      vim.api.nvim_create_autocmd("BufWritePre",
-        {
-          group = auto_group,
-          buffer = bufnr,
-          -- define callback for the event
-          callback = function ()
-            vim.lsp.buf.format({ bufnr = bufnr }) -- call lsp's buffer format 
-                                                  -- command
-          end
-        })
-    end
-  end
 }
 return opts
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,11 @@
-local lazy = require "lazy"
 -- define and return all the plugins to load
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
-    end
+    end,
   },
   -- list nvim plugins to install using 'mason' package manager.
   -- this listed packages can be installed by
@@ -15,17 +14,20 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "lua-language-server", "stylua", -- LSP and formatter for Lua
-        "html-lsp", "css-lsp", "prettier", -- LSP and formatter for html,css
-        "pyright",      -- a lightweight LSP for python
-        "mypy",         -- a null-ls tool for python static type checking
-        "ruff",         -- a null-ls tool for python linting
-        "black",        -- an auto-formating tool for pyhton
-        "clangd",       -- a powerful LSP for C++
-                        -- (includes linting, static analysis, etc.)
+        "lua-language-server",
+        "stylua", -- LSP and formatter for Lua
+        "html-lsp",
+        "css-lsp",
+        "prettier", -- LSP and formatter for html,css
+        "pyright", -- a lightweight LSP for python
+        "mypy", -- a null-ls tool for python static type checking
+        "ruff", -- a null-ls tool for python linting
+        "black", -- an auto-formating tool for pyhton
+        "clangd", -- a powerful LSP for C++
+        -- (includes linting, static analysis, etc.)
         "clang-format", -- a c++ formatter
-      }
-    }
+      },
+    },
   },
   -- setup Syntax highlighters for specific languages using tree-sitter
   -- (i.e. this is equivalent of running `:TSInstall <language_name>`
@@ -34,12 +36,19 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "python", "cpp", "make",
-        "dockerfile", "markdown", "markdown_inline",
-        "html", "css",
-      }
-    }
+        "vim",
+        "lua",
+        "vimdoc",
+        "python",
+        "cpp",
+        "make",
+        "dockerfile",
+        "markdown",
+        "markdown_inline",
+        "html",
+        "css",
+      },
+    },
   },
   -- load the nvchad's lspconfig for lsp setup
   -- and our local 'lspconfig' defined in the 'configs' folder
@@ -48,15 +57,14 @@ return {
     config = function()
       require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
-    end
+    end,
   },
   -- setup null-ls plugin for language tooling such as linters, formatters etc.
   {
     "jose-elias-alvarez/null-ls.nvim",
-    ft = {"python", "cpp"}, -- file types
-    opts = function ()
-      return require "configs.null-ls"  -- load the options from a custom 
-                                        -- 'null-ls' config file
+    opts = function()
+      return require "configs.null-ls" -- load the options from a custom
+      -- 'null-ls' config file
     end,
   },
   -- setup vim tmux navigator plugin
@@ -64,5 +72,5 @@ return {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false, -- always load this plugin
-  }
+  },
 }
